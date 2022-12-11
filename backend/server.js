2794fbd2def4328a1e80e70bb729a2c4ddc53415
rakesh <rnarasimhaiah@livevox.com>
2022-12-11 15:26:13 +0530

    lesson-1 commited

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+
+
+// express app
+const app = express()
+
+app.use((req,res,next)=> {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.get('/',(req,res)=>{
+    res.json({ mssg:'Welcome to the app' })
+})
+
+
+// listen for requests
+app.listen(process.env.PORT, ()=> {
+    console.log('listening on port 4000')
+})
